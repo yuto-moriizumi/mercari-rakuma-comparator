f03f62bffcf760c1bf10542ee86877b744711db3
volgakurvar <kurvan1112@gmail.com>
2023-03-26 14:57:45 +0900

    MercariIconのテストを追加

--- src/__test__/index.spec.tsx
+++ src/__test__/index.spec.tsx
@@ -1,14 +1,26 @@
-/**
- * @jest-environment jsdom
- */
-import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
 import { MercariIcon } from '@/components/MercariIcon';
-import '@testing-library/jest-dom';
 
 describe('Rendering', () => {
-  it('Should render hello text', () => {
+  it('should contain the correct alt text', () => {
     render(<MercariIcon />);
     expect(screen.getByAltText('Mercari icon')).toBeInTheDocument();
+  });
+
+  describe('should display the correct image', () => {
+    it('should display the valid image', () => {
+      render(<MercariIcon valid />);
+      expect(screen.getByAltText('Mercari icon').getAttribute('src')).toContain(
+        'mercari.png'
+      );
+    });
+
+    it('should display the invalid image', () => {
+      render(<MercariIcon />);
+      expect(screen.getByAltText('Mercari icon').getAttribute('src')).toContain(
+        'mercari_gray.png'
+      );
+    });
   });
 });
